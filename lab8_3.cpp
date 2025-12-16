2a69f77324a5570b02669329ab6395ae9bcad886
Jza500 <kittiphob_i@cmu.ac.th>
2025-12-16 16:18:35 +0700

    sedliew sad

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,52 @@
 "Brook"
 "Jinbe"
 */
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Bounty500M(int z){
+    if (z > 500000000)
+    {
+        return "Your character = Jinbe";
+    }else return "Your character = Franky";
+}
+
+string Bounty1_1B(int n){
+    if (n > 1100000000)
+    {
+        return "Your character = Zoro";
+    }else return "Your character = Sanji";
+}
+
+int main(){
+    int age;
+    float height;
+    int bounty;
+
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100)
+        {
+        cout << "Your character = Chopper";
+        }else if (height <180)
+        {
+        cout << "Your character = Usopp";
+        }else {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            cout << Bounty1_1B(bounty);
+        }
+
+    }else if (age <= 60)
+    {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        cout << Bounty500M(bounty);
+    }else cout << "Your character = Brook";
+    
+}
